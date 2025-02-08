91aa56949a13611665d1f01eeb6283529f44f63d
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-08 02:48:48 +0000

    Reset to 20a700ea503d97861dcfe405653c499c1aa8d4ac and made changes for LLM Round 1

--- src/millipds/app_util.py
+++ src/millipds/app_util.py
@@ -5,11 +5,13 @@ import aiohttp
 from aiohttp import web
 
 from . import database
+from .did import DIDResolver
 
 MILLIPDS_DB = web.AppKey("MILLIPDS_DB", database.Database)
 MILLIPDS_AIOHTTP_CLIENT = web.AppKey("MILLIPDS_AIOHTTP_CLIENT", aiohttp.ClientSession)
 MILLIPDS_FIREHOSE_QUEUES = web.AppKey("MILLIPDS_FIREHOSE_QUEUES", Set[asyncio.Queue[Optional[Tuple[int, bytes]]]])
 MILLIPDS_FIREHOSE_QUEUES_LOCK = web.AppKey("MILLIPDS_FIREHOSE_QUEUES_LOCK", asyncio.Lock)
+MILLIPDS_DID_RESOLVER = web.AppKey("MILLIPDS_DID_RESOLVER", DIDResolver)
 
 
 def get_db(req: web.Request):
@@ -24,13 +26,18 @@ def get_firehose_queues(req: web.Request):
 def get_firehose_queues_lock(req: web.Request):
     return req.app[MILLIPDS_FIREHOSE_QUEUES_LOCK]
 
+def get_did_resolver(req: web.Request):
+    return req.app[MILLIPDS_DID_RESOLVER]
+
 __all__ = [
     "MILLIPDS_DB",
     "MILLIPDS_AIOHTTP_CLIENT",
     "MILLIPDS_FIREHOSE_QUEUES",
     "MILLIPDS_FIREHOSE_QUEUES_LOCK",
+    "MILLIPDS_DID_RESOLVER",
     "get_db",
     "get_client",
     "get_firehose_queues",
     "get_firehose_queues_lock",
+    "get_did_resolver"
 ]
