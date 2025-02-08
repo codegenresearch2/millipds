d971d27af178671e28259f519515cf5c15e1f1b3
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-08 03:23:42 +0000

    Reset to 20a700ea503d97861dcfe405653c499c1aa8d4ac and made changes for LLM Round 1

--- src/millipds/static_config.py
+++ src/millipds/static_config.py
@@ -1,13 +1,13 @@
 Hardcoded configs (it is not expected that end-users need to edit this file)
 
-# This is a descriptive comment explaining the purpose of this file.
+# This file contains configuration settings for the application.
 HTTP_LOG_FMT = '%{X-Forwarded-For}i %t (%Tf) "%r" %s %b "%{Referer}i" "%{User-Agent}i"'
 
 GROUPNAME = "millipds-sock"
 
-# Versioning for configurations
-# Bump this version when there are breaking changes to the database schema.
-MILLIPDS_DB_VERSION = 1
+# Versioning for the database, increment this when there are breaking changes to the schema.
+# Current version is 2.
+MILLIPDS_DB_VERSION = 2
 
 # Versioning for ATProto repo
 ATPROTO_REPO_VERSION_3 = 3
@@ -21,7 +21,7 @@ MAIN_DB_PATH = DATA_DIR + "/millipds.sqlite3"
 REPOS_DIR = DATA_DIR + "/repos"
 
 # Queue size for firehose events
-# Adjust this value based on the application's activity level.
+# Adjust this based on the application's activity level.
 FIREHOSE_QUEUE_SIZE = 100
 
 # Time-to-live for DID cache entries
