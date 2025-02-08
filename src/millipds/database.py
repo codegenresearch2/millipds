5330cbc4e47b27f7598af65ec22101357e1758dc
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-08 04:00:46 +0000

    Reset to 90fca00e71475315bd817cae894a97ff197db1ec and made changes for LLM Round 1

--- src/millipds/database.py
+++ src/millipds/database.py
@@ -1,9 +1,26 @@
 import logging
+import apsw
+import argon2
+import cbrrr
+from atmst.blockstore import BlockStore
+from atmst.mst.node import MSTNode
+from . import static_config
+from . import crypto
+
+logger = logging.getLogger(__name__)
 
 class Database:
     def __init__(self, path):
         self.path = path
         self.config = {}
+        self.pw_hasher = argon2.PasswordHasher()
+        self.con = self._new_con()
+
+    def _new_con(self, readonly=False):
+        return apsw.Connection(
+            self.path,
+            flags=(apsw.SQLITE_OPEN_READONLY if readonly else apsw.SQLITE_OPEN_READWRITE | apsw.SQLITE_OPEN_CREATE)
+        )
 
     def update_config(self, pds_pfx=None, pds_did=None, bsky_appview_pfx=None, bsky_appview_did=None):
         if pds_pfx is not None:
@@ -15,36 +32,48 @@ class Database:
         if bsky_appview_did is not None:
             self.config['bsky_appview_did'] = bsky_appview_did
 
-    def get_config(self):
-        return self.config
+    @property
+    def config(self):
+        return self._config
 
-class DBBlockStore(Database):
-    def __init__(self, db_connection, repo_id):
-        super().__init__(path)
-        self.db_connection = db_connection
-        self.repo_id = repo_id
-        self.logger = logging.getLogger(__name__)
+    @config.setter
+    def config(self, value):
+        self._config = value
 
-    def get_block(self, key):
-        try:
-            # Implementation for retrieving a block
-            self.logger.info(f'Retrieving block with key: {key}')
-        except Exception as e:
-            self.logger.error(f'Error retrieving block with key: {key} - {e}')
-            raise
+    def create_account(self, did, handle, password, privkey):
+        pw_hash = self.pw_hasher.hash(password)
+        privkey_pem = crypto.privkey_to_pem(privkey)
+        logger.info(f'Creating account for did={did}, handle={handle}')
 
-    def put_block(self, key, value):
-        try:
-            # Implementation for putting a block
-            self.logger.info(f'Putting block with key: {key} and value: {value}')
-        except Exception as e:
-            self.logger.error(f'Error putting block with key: {key} - {e}')
-            raise
-
-    def del_block(self, key):
-        try:
-            # Implementation for deleting a block
-            self.logger.info(f'Deleting block with key: {key}')
-        except Exception as e:
-            self.logger.error(f'Error deleting block with key: {key} - {e}')
-            raise
+        with self.con:
+            tid = util.tid_now()
+            empty_mst = MSTNode.empty_root()
+            initial_commit = {
+                'did': did,
+                'version': static_config.ATPROTO_REPO_VERSION_3,
+                'data': empty_mst.cid,
+                'rev': tid,
+                'prev': None,
+            }
+            initial_commit['sig'] = crypto.raw_sign(
+                privkey, cbrrr.encode_dag_cbor(initial_commit)
+            )
+            commit_bytes = cbrrr.encode_dag_cbor(initial_commit)
+            commit_cid = cbrrr.CID.cidv1_dag_cbor_sha256_32_from(commit_bytes)
+            self.con.execute(
+                '''
+                INSERT INTO user(
+                    did,
+                    handle,
+                    prefs,
+                    pw_hash,
+                    signing_key,
+                    head,
+                    rev,
+                    commit_bytes
+                ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+                ''',
+                (
+                    did,
+                    handle,
+                    b'{}\
