5856dc6f61c26d2b3fcc3d7a38f172496411556f
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-08 02:36:55 +0000

    Reset to 90fca00e71475315bd817cae894a97ff197db1ec and made changes for LLM Round 1

--- src/millipds/ssrf.py
+++ src/millipds/ssrf.py
@@ -1,10 +1,3 @@
-"""
-This is a bit of a bodge, for now.
-
-See https://github.com/aio-libs/aiohttp/discussions/10224 for the discussion
-that led to this, and maybe a better solution in the future.
-"""
-
 import ipaddress
 from aiohttp import TCPConnector, ClientSession
 import aiohttp.connector
@@ -14,29 +7,24 @@ from aiohttp.resolver import DefaultResolver, AbstractResolver
 # (without this, bare IPs in the URL will bypass the resolver, where our SSRF check is)
 aiohttp.connector.is_ip_address = lambda _: False
 
-
 class SSRFException(ValueError):
-	pass
-
+    pass
 
 class SSRFSafeResolverWrapper(AbstractResolver):
-	def __init__(self, resolver: AbstractResolver):
-		self.resolver = resolver
+    def __init__(self, resolver: AbstractResolver):
+        self.resolver = resolver
 
-	async def resolve(self, host: str, port: int, family: int):
-		result = await self.resolver.resolve(host, port, family)
-		for host in result:
-			if ipaddress.ip_address(host["host"]).is_private:
-				raise SSRFException(
-					"Can't connect to private IP: " + host["host"]
-				)
-		return result
-
-	async def close(self) -> None:
-		await self.resolver.close()
-
+    async def resolve(self, host: str, port: int, family: int):
+        result = await self.resolver.resolve(host, port, family)
+        for host in result:
+            if ipaddress.ip_address(host['host']).is_private:
+                raise SSRFException('Can\'t connect to private IP: ' + host['host'])
+        return result
+    
+    async def close(self) -> None:
+        await self.resolver.close()
 
 def get_ssrf_safe_client() -> ClientSession:
-	resolver = SSRFSafeResolverWrapper(DefaultResolver())
-	connector = TCPConnector(resolver=resolver)
-	return ClientSession(connector=connector)
+    resolver = SSRFSafeResolverWrapper(DefaultResolver())
+    connector = TCPConnector(resolver=resolver)
+    return ClientSession(connector=connector)
