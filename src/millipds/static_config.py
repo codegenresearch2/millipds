2b7cfae4b6150ee7c11ecf9db543f559366e7bb0
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-11 15:47:57 +0000

    Reset to 20a700ea503d97861dcfe405653c499c1aa8d4ac and made changes for LLM Round 1

--- src/millipds/static_config.py
+++ src/millipds/static_config.py
@@ -3,17 +3,15 @@
 # (some of this stuff might want to be broken out into a proper config file, eventually)
 
 # Format for HTTP access logs
-HTTP_LOG_FMT = (
-    '%{X-Forwarded-For}i %t (%Tf) "%r" %s %b "%{Referer}i" "%{User-Agent}i"'
-)
+HTTP_LOG_FMT = '%{X-Forwarded-For}i %t (%Tf) "%r" %s %b "%{Referer}i" "%{User-Agent}i"'
 
 # Group name for the socket
 GROUPNAME = "millipds-sock"
 
-# Version of the database schema
+# Version of the database schema (gets bumped with breaking changes)
 MILLIPDS_DB_VERSION = 2
 
-# Version of the ATProto repository
+# Version of the ATProto repository (might change if the ATProto spec changes)
 ATPROTO_REPO_VERSION_3 = 3
 
 # Version of the CAR file format
@@ -28,7 +26,7 @@ MAIN_DB_PATH = DATA_DIR + "/millipds.sqlite3"
 # Directory for repository storage
 REPOS_DIR = DATA_DIR + "/repos"
 
-# Maximum size of the firehose queue
+# Maximum size of the firehose queue (potentially increase for very active PDS instances)
 FIREHOSE_QUEUE_SIZE = 100
 
 # Time-to-live for DID cache in seconds
@@ -39,5 +37,3 @@ DID_CACHE_ERROR_TTL = 60 * 5  # 5 minutes
 
 # Host for the PLC directory
 PLC_DIRECTORY_HOST = "https://your_plc_directory_host"
-
-I have addressed the feedback provided by the oracle. I have updated the comments to be more concise and consistent with the gold code. I have also added parentheses for the multi-line string `HTTP_LOG_FMT` for better readability. I have updated the value for `PLC_DIRECTORY_HOST` to include the protocol. I have also added a comment about firehose events to enhance understanding.
