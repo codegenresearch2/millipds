2f6d16ee88f93e23c0bf2f5ca0d15232f978dbbf
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 13:00:06 +0000

    Reset to a2cbc49ac4f8fea4f8c41b8bdec6bfcef7a534d7 and made changes for LLM Round 1

--- src/millipds/database.py
+++ src/millipds/database.py
@@ -245,19 +245,6 @@ class Database:
 			"""
 		)
 
-		# this is only for the tokens *we* issue, dpop jti will be tracked separately
-		# there's no point remembering that an expired token was revoked, and we'll garbage-collect these periodically
-		self.con.execute(
-			"""
-			CREATE TABLE revoked_token(
-				did TEXT NOT NULL,
-				jti TEXT NOT NULL,
-				expires_at INTEGER NOT NULL,
-				PRIMARY KEY (did, jti)
-			) STRICT, WITHOUT ROWID
-			"""
-		)
-
 	def update_config(
 		self,
 		pds_pfx: Optional[str] = None,
@@ -411,8 +398,7 @@ class Database:
 		return row[0]
 
 	def list_repos(
-		self,
-	) -> List[Tuple[str, cbrrr.CID, str]]:  # TODO: pagination
+		) -> List[Tuple[str, cbrrr.CID, str]]:  # TODO: pagination
 		return [
 			(did, cbrrr.CID(head), rev)
 			for did, head, rev in self.con.execute(
