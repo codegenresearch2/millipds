b75552b228d3a877aa091183495c7ad3d9cd671b
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 10:00:37 +0000

    Reset to 20a700ea503d97861dcfe405653c499c1aa8d4ac and made changes for LLM Round 1

--- src/millipds/static_config.py
+++ src/millipds/static_config.py
@@ -1,69 +1,37 @@
-"""
-Hardcoded configs (it is not expected that end-users need to edit this file)
+# Hardcoded configs (it is not expected that end-users need to edit this file)
 
-(some of this stuff might want to be broken out into a proper config file, eventually)
-"""
+# Logging format for HTTP requests
+HTTP_LOG_FMT = '%{X-Forwarded-For}i %t (%Tf) "%r" %s %b "%{Referer}i" "%{User-Agent}i"'
 
-HTTP_LOG_FMT = (
-	'%{X-Forwarded-For}i %t (%Tf) "%r" %s %b "%{Referer}i" "%{User-Agent}i"'
-)
-
+# Group name for Unix socket permissions
 GROUPNAME = "millipds-sock"
 
-MILLIPDS_DB_VERSION = (
-	1  # this gets bumped if we make breaking changes to the db schema
-)
-ATPROTO_REPO_VERSION_3 = 3  # might get bumped if the atproto spec changes
+# Database version
+MILLIPDS_DB_VERSION = 2  # This gets bumped if we make breaking changes to the db schema
+
+# Version for ATProto repo
+ATPROTO_REPO_VERSION_3 = 3  # Might get bumped if the atproto spec changes
+
+# Version for CAR files
 CAR_VERSION_1 = 1
 
+# Directory for data storage
 DATA_DIR = "./data"
+
+# Path to the main database file
 MAIN_DB_PATH = DATA_DIR + "/millipds.sqlite3"
+
+# Directory for repository storage
 REPOS_DIR = DATA_DIR + "/repos"
 
-# might want to tweak this upwards on a very active PDS
+# Size of the firehose queue
 FIREHOSE_QUEUE_SIZE = 100
 
-# NB: each firehose event can be up to ~1MB, but on average they're much smaller
-
+# Time-to-live for DID cache entries
 DID_CACHE_TTL = 60 * 60  # 1 hour
+
+# Time-to-live for DID cache entries in case of error
 DID_CACHE_ERROR_TTL = 60 * 5  # 5 mins
 
-# New functionality
-from .did import DIDResolver
-
-MILLIPDS_DID_RESOLVER = web.AppKey("MILLIPDS_DID_RESOLVER", DIDResolver)
-
-# Updated __all__ to include new items
-__all__ = [
-	"MILLIPDS_DB",
-	"MILLIPDS_AIOHTTP_CLIENT",
-	"MILLIPDS_FIREHOSE_QUEUES",
-	"MILLIPDS_FIREHOSE_QUEUES_LOCK",
-	"MILLIPDS_DID_RESOLVER",
-	"get_db",
-	"get_client",
-	"get_firehose_queues",
-	"get_firehose_queues_lock",
-	"get_did_resolver",
-]
-
-# Maintaining consistent naming conventions for helpers
-def get_did_resolver(req: web.Request):
-	return req.app[MILLIPDS_DID_RESOLVER]
-
-# Including a DID resolver instance
-did_resolver = DIDResolver(get_client(request), static_config.PLC_DIRECTORY_HOST)
-app[MILLIPDS_DID_RESOLVER] = did_resolver
-
-# Maintaining existing middleware and routes
-cors = cors_middleware(
-	allow_all=True,
-	expose_headers=["*"],
-	allow_headers=["*"],
-	allow_methods=["*"],
-	allow_credentials=True,
-	max_age=100_000_000,
-)
-
-app.middlewares.append(cors)
-app.middlewares.append(atproto_service_proxy_middleware)
+# Host for PLC directory
+PLC_DIRECTORY_HOST = "https://plc.directory"
