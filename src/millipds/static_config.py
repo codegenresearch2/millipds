cb6b6b501c8870331c21dddfa0be7f753e1ea12e
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 10:19:47 +0000

    Reset to 20a700ea503d97861dcfe405653c499c1aa8d4ac and made changes for LLM Round 1

--- src/millipds/static_config.py
+++ src/millipds/static_config.py
@@ -4,11 +4,12 @@ Hardcoded configs (it is not expected that end-users need to edit this file)
 (some of this stuff might want to be broken out into a proper config file, eventually)
 """
 
-HTTP_LOG_FMT = '%{X-Forwarded-For}i %t (%Tf) "%r" %s %b "%{Referer}i" "%{User-Agent}i'
+HTTP_LOG_FMT = (
+    '%{X-Forwarded-For}i %t (%Tf) "%r" %s %b "%{Referer}i" "%{User-Agent}i'
+)
 
 GROUPNAME = "millipds-sock"
 
-# Ensure comments are placed directly above the relevant lines of code
 MILLIPDS_DB_VERSION = 2  # this gets bumped if we make breaking changes to the db schema
 
 ATPROTO_REPO_VERSION_3 = 3  # might get bumped if the atproto spec changes
@@ -18,13 +19,9 @@ DATA_DIR = "./data"
 MAIN_DB_PATH = DATA_DIR + "/millipds.sqlite3"
 REPOS_DIR = DATA_DIR + "/repos"
 
-# might want to tweak this upwards on a very active PDS
-FIREHOSE_QUEUE_SIZE = 100
-
-# NB: each firehose event can be up to ~1MB, but on average they're much smaller
+FIREHOSE_QUEUE_SIZE = 100  # might want to tweak this upwards on a very active PDS
 
 DID_CACHE_TTL = 60 * 60  # 1 hour
 DID_CACHE_ERROR_TTL = 60 * 5  # 5 mins
 
-# Add the missing configuration variable
-PLC_DIRECTORY_HOST = "https://plc.directory"
+PLC_DIRECTORY_HOST = "https://plc.directory"  # Add the missing configuration variable
