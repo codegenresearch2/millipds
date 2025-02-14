47d3ddcfd2ed00ec0ea69efc346cdf7479146372
vatsal_venkatkrishna <vatsal_venkatkrishna@gcpl4-eu-0>
2025-02-14 12:38:37 +0000

    Reset to a2cbc49ac4f8fea4f8c41b8bdec6bfcef7a534d7 and made changes for LLM Round 1

--- src/millipds/database.py
+++ src/millipds/database.py
@@ -1,9 +1,4 @@
-"""
-Ideally, all SQL statements are contained within this file.
-
-Password hashing also happens in here, because it doesn't make much sense to do
-it anywhere else.
-"""
+"""\nIdeally, all SQL statements are contained within this file.\n\nPassword hashing also happens in here, because it doesn't make much sense to do\nit anywhere else.\n"""
 
 from typing import Optional, Dict, List, Tuple
 from functools import cached_property
@@ -29,9 +24,7 @@ apsw.bestpractice.apply(apsw.bestpractice.recommended)
 
 
 class DBBlockStore(BlockStore):
-	"""
-	Adapt the db for consumption by the atmst library
-	"""
+	"""\n	Adapt the db for consumption by the atmst library\n	"""
 
 	def __init__(self, db: apsw.Connection, repo: str) -> None:
 		self.db = db
@@ -77,29 +70,11 @@ class Database:
 				self._init_tables()
 
 	def new_con(self, readonly=False):
-		"""
-		https://rogerbinns.github.io/apsw/cursor.html
-		"Cursors on the same Connection are not isolated from each other.
+		"""\n		https://rogerbinns.github.io/apsw/cursor.html\n		"Cursors on the same Connection are not isolated from each other.
 		Anything done on one cursor is immediately visible to all other Cursors
 		on the same connection. This still applies if you start transactions.
-		Connections are isolated from each other with cursors on other
-		connections not seeing changes until they are committed."
-
-		therefore we frequently spawn new connections when we need an isolated cursor
-		"""
-		return apsw.Connection(
-			self.path,
-			flags=(
-				apsw.SQLITE_OPEN_READONLY
-				if readonly
-				else apsw.SQLITE_OPEN_READWRITE | apsw.SQLITE_OPEN_CREATE
-			),
-		)
-
-	def _init_tables(self):
-		logger.info("initing tables")
-		self.con.execute(
-			"""
+		Therefore we frequently spawn new connections when we need an isolated cursor
+		"""\n		return apsw.Connection(\n			self.path,\n			flags=(\n				apsw.SQLITE_OPEN_READONLY\n				if readonly\n				else apsw.SQLITE_OPEN_READWRITE | apsw.SQLITE_OPEN_CREATE\n			),\n		)\n\n	def _init_tables(self):\n		logger.info("initing tables")\n		self.con.execute(\n			"""
 			CREATE TABLE config(
 				db_version INTEGER NOT NULL,
 				pds_pfx TEXT,
@@ -108,22 +83,12 @@ class Database:
 				bsky_appview_did TEXT,
 				jwt_access_secret TEXT NOT NULL
 			) STRICT
-			"""
-		)
-
-		self.con.execute(
-			"""
+			"""\n		)\n\n		self.con.execute(\n			"""
 			INSERT INTO config(
 				db_version,
 				jwt_access_secret
 			) VALUES (?, ?)
-			""",
-			(static_config.MILLIPDS_DB_VERSION, secrets.token_hex()),
-		)
-
-		# TODO: head and rev are redundant, technically (rev contained within commit_bytes)
-		self.con.execute(
-			"""
+			""",\n			(static_config.MILLIPDS_DB_VERSION, secrets.token_hex()),\n		)\n\n		self.con.execute(\n			"""
 			CREATE TABLE user(
 				id INTEGER PRIMARY KEY AUTOINCREMENT,
 				did TEXT NOT NULL,
@@ -135,25 +100,13 @@ class Database:
 				rev TEXT NOT NULL,
 				commit_bytes BLOB NOT NULL
 			) STRICT
-			"""
-		)
-
-		self.con.execute("CREATE UNIQUE INDEX user_by_did ON user(did)")
-		self.con.execute("CREATE UNIQUE INDEX user_by_handle ON user(handle)")
-
-		self.con.execute(
-			"""
+			"""\n		)\n\n		self.con.execute("CREATE UNIQUE INDEX user_by_did ON user(did)")\n		self.con.execute("CREATE UNIQUE INDEX user_by_handle ON user(handle)")\n\n		self.con.execute(\n			"""
 			CREATE TABLE firehose(
 				seq INTEGER PRIMARY KEY AUTOINCREMENT,
 				timestamp INTEGER NOT NULL,
 				msg BLOB NOT NULL
 			) STRICT
-			"""
-		)
-
-		# repo storage stuff
-		self.con.execute(
-			"""
+			"""\n		)\n\n		self.con.execute(\n			"""
 			CREATE TABLE mst(
 				repo INTEGER NOT NULL,
 				cid BLOB NOT NULL,
@@ -162,13 +115,7 @@ class Database:
 				FOREIGN KEY (repo) REFERENCES user(id),
 				PRIMARY KEY (repo, cid)
 			) STRICT, WITHOUT ROWID
-			"""
-		)
-		# should maybe be (repo, since) instead?
-		self.con.execute("CREATE INDEX mst_since ON mst(since)")
-
-		self.con.execute(
-			"""
+			"""\n		)\n		self.con.execute("CREATE INDEX mst_since ON mst(since)")\n\n		self.con.execute(\n			"""
 			CREATE TABLE record(
 				repo INTEGER NOT NULL,
 				nsid TEXT NOT NULL,
@@ -179,19 +126,7 @@ class Database:
 				FOREIGN KEY (repo) REFERENCES user(id),
 				PRIMARY KEY (repo, nsid, rkey)
 			) STRICT, WITHOUT ROWID
-			"""
-		)
-		# should maybe be (repo, since) instead? maybe also (repo, nsid, since)?
-		self.con.execute("CREATE INDEX record_since ON record(since)")
-
-		# nb: blobs are partitioned per-repo
-		# TODO: think carefully about refcount/since interaction?
-		# TODO: when should blob GC happen? after each commit? (nah, that would behave badly with e.g. concurrent browser sessions)
-		# NOTE: blobs have null cid when they're midway through being uploaded,
-		# and they have null "since" when they haven't been committed yet
-		# TODO: store length explicitly?
-		self.con.execute(
-			"""
+			"""\n		)\n		self.con.execute("CREATE INDEX record_since ON record(since)")\n\n		self.con.execute(\n			"""
 			CREATE TABLE blob(
 				id INTEGER PRIMARY KEY AUTOINCREMENT,
 				repo INTEGER NOT NULL,
@@ -200,16 +135,7 @@ class Database:
 				since TEXT,
 				FOREIGN KEY (repo) REFERENCES user(id)
 			) STRICT
-			"""
-		)
-		self.con.execute(
-			"CREATE INDEX blob_isrefd ON blob(refcount, refcount > 0)"
-		)  # dunno how useful this is
-		self.con.execute("CREATE UNIQUE INDEX blob_repo_cid ON blob(repo, cid)")
-		self.con.execute("CREATE INDEX blob_since ON blob(since)")
-
-		self.con.execute(
-			"""
+			"""\n		)\n		self.con.execute(\n			"CREATE INDEX blob_isrefd ON blob(refcount, refcount > 0)"\n		)\n		self.con.execute("CREATE UNIQUE INDEX blob_repo_cid ON blob(repo, cid)")\n		self.con.execute("CREATE INDEX blob_since ON blob(since)")\n\n		self.con.execute(\n			"""
 			CREATE TABLE blob_part(
 				blob INTEGER NOT NULL,
 				idx INTEGER NOT NULL,
@@ -217,131 +143,21 @@ class Database:
 				PRIMARY KEY (blob, idx),
 				FOREIGN KEY (blob) REFERENCES blob(id)
 			) STRICT, WITHOUT ROWID
-			"""
-		)
-
-		# we cache failures too, represented as a null doc (with shorter TTL)
-		# timestamps are unix timestamp ints, in seconds
-		self.con.execute(
-			"""
+			"""\n		)\n\n		self.con.execute(\n			"""
 			CREATE TABLE did_cache(
 				did TEXT PRIMARY KEY NOT NULL,
 				doc BLOB,
 				created_at INTEGER NOT NULL,
 				expires_at INTEGER NOT NULL
 			) STRICT, WITHOUT ROWID
-			"""
-		)
-
-		# likewise, a null did represents a failed resolution
-		self.con.execute(
-			"""
+			"""\n		)\n\n		self.con.execute(\n			"""
 			CREATE TABLE handle_cache(
 				handle TEXT PRIMARY KEY NOT NULL,
 				did TEXT,
 				created_at INTEGER NOT NULL,
 				expires_at INTEGER NOT NULL
 			) STRICT, WITHOUT ROWID
-			"""
-		)
-
-		# this is only for the tokens *we* issue, dpop jti will be tracked separately
-		# there's no point remembering that an expired token was revoked, and we'll garbage-collect these periodically
-		self.con.execute(
-			"""
-			CREATE TABLE revoked_token(
-				did TEXT NOT NULL,
-				jti TEXT NOT NULL,
-				expires_at INTEGER NOT NULL,
-				PRIMARY KEY (did, jti)
-			) STRICT, WITHOUT ROWID
-			"""
-		)
-
-	def update_config(
-		self,
-		pds_pfx: Optional[str] = None,
-		pds_did: Optional[str] = None,
-		bsky_appview_pfx: Optional[str] = None,
-		bsky_appview_did: Optional[str] = None,
-	):
-		with self.con:
-			if pds_pfx is not None:
-				self.con.execute("UPDATE config SET pds_pfx=?", (pds_pfx,))
-			if pds_did is not None:
-				self.con.execute("UPDATE config SET pds_did=?", (pds_did,))
-			if bsky_appview_pfx is not None:
-				self.con.execute(
-					"UPDATE config SET bsky_appview_pfx=?", (bsky_appview_pfx,)
-				)
-			if bsky_appview_did is not None:
-				self.con.execute(
-					"UPDATE config SET bsky_appview_did=?", (bsky_appview_did,)
-				)
-
-		try:
-			del self.config  # invalidate the cached value
-		except AttributeError:
-			pass
-
-	@cached_property
-	def config(self) -> Dict[str, object]:
-		config_fields = (
-			"db_version",
-			"pds_pfx",
-			"pds_did",
-			"bsky_appview_pfx",
-			"bsky_appview_did",
-			"jwt_access_secret",
-		)
-
-		cfg = self.con.execute(
-			f"SELECT {', '.join(config_fields)} FROM config"
-		).fetchone()
-
-		# TODO: consider using a properly typed dataclass rather than a dict
-		# see also https://docs.python.org/3/library/typing.html#typing.TypedDict
-		return dict(zip(config_fields, cfg))
-
-	def config_is_initialised(self) -> bool:
-		return all(v is not None for v in self.config.values())
-
-	def print_config(self, redact_secrets: bool = True) -> None:
-		maxlen = max(map(len, self.config))
-		for k, v in self.config.items():
-			if redact_secrets and "secret" in k:
-				v = "[REDACTED]"
-			print(f"{k:<{maxlen}} : {v!r}")
-
-	def create_account(
-		self,
-		did: str,
-		handle: str,
-		password: str,
-		privkey: crypto.ec.EllipticCurvePrivateKey,
-	) -> None:
-		pw_hash = self.pw_hasher.hash(password)
-		privkey_pem = crypto.privkey_to_pem(privkey)
-		logger.info(f"creating account for did={did}, handle={handle}")
-
-		# create an initial commit for an empty MST, as an atomic transaction
-		with self.con:
-			tid = util.tid_now()
-			empty_mst = MSTNode.empty_root()
-			initial_commit = {
-				"did": did,  # TODO: did normalisation, somewhere?
-				"version": static_config.ATPROTO_REPO_VERSION_3,
-				"data": empty_mst.cid,
-				"rev": tid,
-				"prev": None,
-			}
-			initial_commit["sig"] = crypto.raw_sign(
-				privkey, cbrrr.encode_dag_cbor(initial_commit)
-			)
-			commit_bytes = cbrrr.encode_dag_cbor(initial_commit)
-			commit_cid = cbrrr.CID.cidv1_dag_cbor_sha256_32_from(commit_bytes)
-			self.con.execute(
-				"""
+			"""\n		)\n\n	def update_config(\n		self,\n		pds_pfx: Optional[str] = None,\n		pds_did: Optional[str] = None,\n		bsky_appview_pfx: Optional[str] = None,\n		bsky_appview_did: Optional[str] = None,\n	):\n		with self.con:\n			if pds_pfx is not None:\n				self.con.execute("UPDATE config SET pds_pfx=?", (pds_pfx,))\n			if pds_did is not None:\n				self.con.execute("UPDATE config SET pds_did=?", (pds_did,))\n			if bsky_appview_pfx is not None:\n				self.con.execute(\n					"UPDATE config SET bsky_appview_pfx=?", (bsky_appview_pfx,)\n				)\n			if bsky_appview_did is not None:\n				self.con.execute(\n					"UPDATE config SET bsky_appview_did=?", (bsky_appview_did,)\n				)\n\n		try:\n			del self.config  # invalidate the cached value\n		except AttributeError:\n			pass\n\n	@cached_property\n	def config(self) -> Dict[str, object]:\n		config_fields = (\n			"db_version",\n			"pds_pfx",\n			"pds_did",\n			"bsky_appview_pfx",\n			"bsky_appview_did",\n			"jwt_access_secret",\n		)\n\n		cfg = self.con.execute(\n			f"SELECT {', '.join(config_fields)} FROM config"\n		).fetchone()\n\n		return dict(zip(config_fields, cfg))\n\n	def config_is_initialised(self) -> bool:\n		return all(v is not None for v in self.config.values())\n\n	def print_config(self, redact_secrets: bool = True) -> None:\n		maxlen = max(map(len, self.config))\n		for k, v in self.config.items():\n			if redact_secrets and "secret" in k:\n				v = "[REDACTED]"\n			print(f"{k:<{maxlen}} : {v!r}")\n\n	def create_account(\n		self,\n		did: str,\n		handle: str,\n		password: str,\n		privkey: crypto.ec.EllipticCurvePrivateKey,\n	) -> None:\n		pw_hash = self.pw_hasher.hash(password)\n		privkey_pem = crypto.privkey_to_pem(privkey)\n		logger.info(f"creating account for did={did}, handle={handle}")\n\n		with self.con:\n			tid = util.tid_now()\n			empty_mst = MSTNode.empty_root()\n			initial_commit = {\n				"did": did,\n				"version": static_config.ATPROTO_REPO_VERSION_3,\n				"data": empty_mst.cid,\n				"rev": tid,\n				"prev": None,\n				"jti": secrets.token_hex(),  # Include 'jti' in the JWT\n				"sub": did,  # Include 'sub' in the JWT\n			}\n			initial_commit["sig"] = crypto.raw_sign(\n				privkey, cbrrr.encode_dag_cbor(initial_commit)\n			)\n			commit_bytes = cbrrr.encode_dag_cbor(initial_commit)\n			commit_cid = cbrrr.CID.cidv1_dag_cbor_sha256_32_from(commit_bytes)\n			self.con.execute(\n				"""
 				INSERT INTO user(
 					did,
 					handle,
@@ -352,73 +168,4 @@ class Database:
 					rev,
 					commit_bytes
 				) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-				""",
-				(
-					did,
-					handle,
-					b'{"preferences":[]}',
-					pw_hash,
-					privkey_pem,
-					bytes(commit_cid),
-					tid,
-					commit_bytes,
-				),
-			)
-			user_id = self.con.last_insert_rowid()
-			self.con.execute(
-				"INSERT INTO mst(repo, cid, since, value) VALUES (?, ?, ?, ?)",
-				(user_id, bytes(empty_mst.cid), tid, empty_mst.serialised),
-			)
-
-	def verify_account_login(
-		self, did_or_handle: str, password: str
-	) -> Tuple[str, str, str, str]:
-		row = self.con.execute(
-			"SELECT did, handle, pw_hash FROM user WHERE did=? OR handle=?",
-			(did_or_handle, did_or_handle),
-		).fetchone()
-		if row is None:
-			raise KeyError("no account found for did")
-		did, handle, pw_hash = row
-		try:
-			self.pw_hasher.verify(pw_hash, password)
-		except argon2.exceptions.VerifyMismatchError:
-			raise ValueError("invalid password")
-		return did, handle
-
-	def did_by_handle(self, handle: str) -> Optional[str]:
-		row = self.con.execute(
-			"SELECT did FROM user WHERE handle=?", (handle,)
-		).fetchone()
-		if row is None:
-			return None
-		return row[0]
-
-	def handle_by_did(self, did: str) -> Optional[str]:
-		row = self.con.execute(
-			"SELECT handle FROM user WHERE did=?", (did,)
-		).fetchone()
-		if row is None:
-			return None
-		return row[0]
-
-	def signing_key_pem_by_did(self, did: str) -> Optional[str]:
-		row = self.con.execute(
-			"SELECT signing_key FROM user WHERE did=?", (did,)
-		).fetchone()
-		if row is None:
-			return None
-		return row[0]
-
-	def list_repos(
-		self,
-	) -> List[Tuple[str, cbrrr.CID, str]]:  # TODO: pagination
-		return [
-			(did, cbrrr.CID(head), rev)
-			for did, head, rev in self.con.execute(
-				"SELECT did, head, rev FROM user"
-			).fetchall()
-		]
-
-	def get_blockstore(self, did: str) -> "Database":
-		return DBBlockStore(self, did)
+				""",\n				(\n					did,\n					handle,\n					b'{"preferences":[]}',\n					pw_hash,\n					privkey_pem,\n					bytes(commit_cid),\n					tid,\n					commit_bytes,\n				),\n			)\n			user_id = self.con.last_insert_rowid()\n			self.con.execute(\n				"INSERT INTO mst(repo, cid, since, value) VALUES (?, ?, ?, ?)",\n				(user_id, bytes(empty_mst.cid), tid, empty_mst.serialised),\n			)\n\n	def verify_account_login(\n		self, did_or_handle: str, password: str\n	) -> Tuple[str, str, str, str]:\n		row = self.con.execute(\n			"SELECT did, handle, pw_hash FROM user WHERE did=? OR handle=?",\n			(did_or_handle, did_or_handle),\n		).fetchone()\n		if row is None:\n			raise KeyError("no account found for did")\n		did, handle, pw_hash = row\n		try:\n			self.pw_hasher.verify(pw_hash, password)\n		except argon2.exceptions.VerifyMismatchError:\n			raise ValueError("invalid password")\n		return did, handle\n\n	def did_by_handle(self, handle: str) -> Optional[str]:\n		row = self.con.execute(\n			"SELECT did FROM user WHERE handle=?", (handle,)\n		).fetchone()\n		if row is None:\n			return None\n		return row[0]\n\n	def handle_by_did(self, did: str) -> Optional[str]:\n		row = self.con.execute(\n			"SELECT handle FROM user WHERE did=?", (did,)\n		).fetchone()\n		if row is None:\n			return None\n		return row[0]\n\n	def signing_key_pem_by_did(self, did: str) -> Optional[str]:\n		row = self.con.execute(\n			"SELECT signing_key FROM user WHERE did=?", (did,)\n		).fetchone()\n		if row is None:\n			return None\n		return row[0]\n\n	def list_repos(\n		self,\n	) -> List[Tuple[str, cbrrr.CID, str]]:  # TODO: pagination\n		return [\n			(did, cbrrr.CID(head), rev)\n			for did, head, rev in self.con.execute(\n				"SELECT did, head, rev FROM user"\n			).fetchall()\n		]\n\n	def get_blockstore(self, did: str) -> "Database":\n		return DBBlockStore(self, did)
