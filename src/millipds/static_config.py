b7106a3429ab354cc348d307f61e1b22d38a15d7
vatsal_venkatkrishna <vatsal_venkatkrishna@gcpl4-eu-0>
2025-02-14 11:31:46 +0000

    Reset to 20a700ea503d97861dcfe405653c499c1aa8d4ac and made changes for LLM Round 1

--- src/millipds/static_config.py
+++ src/millipds/static_config.py
@@ -1,8 +1,4 @@
-"""
-Hardcoded configs (it is not expected that end-users need to edit this file)
-
-(some of this stuff might want to be broken out into a proper config file, eventually)
-"""
+"""\nHardcoded configs (it is not expected that end-users need to edit this file)\n\n(some of this stuff might want to be broken out into a proper config file, eventually)\n"""
 
 HTTP_LOG_FMT = (
 	'%{X-Forwarded-For}i %t (%Tf) "%r" %s %b "%{Referer}i" "%{User-Agent}i"'
@@ -10,9 +6,9 @@ HTTP_LOG_FMT = (
 
 GROUPNAME = "millipds-sock"
 
-# this gets bumped if we make breaking changes to the db schema
-MILLIPDS_DB_VERSION = 2
-
+MILLIPDS_DB_VERSION = (
+	1  # this gets bumped if we make breaking changes to the db schema
+)
 ATPROTO_REPO_VERSION_3 = 3  # might get bumped if the atproto spec changes
 CAR_VERSION_1 = 1
 
@@ -23,9 +19,4 @@ REPOS_DIR = DATA_DIR + "/repos"
 # might want to tweak this upwards on a very active PDS
 FIREHOSE_QUEUE_SIZE = 100
 
-# NB: each firehose event can be up to ~1MB, but on average they're much smaller
-
-DID_CACHE_TTL = 60 * 60  # 1 hour
-DID_CACHE_ERROR_TTL = 60 * 5  # 5 mins
-
-PLC_DIRECTORY_HOST = "https://plc.directory"
+# NB: each firehose event can be up to ~1MB, but on average they're much smaller\n\nDID_CACHE_TTL = 60 * 60  # 1 hour\nDID_CACHE_ERROR_TTL = 60 * 5  # 5 mins\n\n# New functionality\nfrom .did import DIDResolver\n\n__all__ = [\n	"HTTP_LOG_FMT",\n	"GROUPNAME",\n	"MILLIPDS_DB_VERSION",\n	"ATPROTO_REPO_VERSION_3",\n	"CAR_VERSION_1",\n	"DATA_DIR",\n	"MAIN_DB_PATH",\n	"REPOS_DIR",\n	"FIREHOSE_QUEUE_SIZE",\n	"DID_CACHE_TTL",\n	"DID_CACHE_ERROR_TTL",\n	"DIDResolver",\n]\n\n# Include a DID resolver instance\ndid_resolver = DIDResolver()
